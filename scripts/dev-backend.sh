eb16ff25cda545d2e5a44f330d14508ac3830d88
Krzysztof Wende <wende@hey.com>
2025-12-11 20:22:52 +0100

    Try make dev (vk/78ff-try-make-dev)
    If anything goes wrong report before acting

--- scripts/dev-backend.sh
+++ scripts/dev-backend.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Run in login shell to inherit user's environment (asdf, mise, nvm, etc.)
-exec ${SHELL:-bash} -lc 'DISABLE_WORKTREE_ORPHAN_CLEANUP=1 RUST_LOG=debug cargo watch -w crates -x "run --bin server"'
+# Pass BACKEND_PORT through to ensure the server uses the allocated port
+exec ${SHELL:-bash} -lc "BACKEND_PORT=${BACKEND_PORT:-0} DISABLE_WORKTREE_ORPHAN_CLEANUP=1 RUST_LOG=debug cargo watch -w crates -x 'run --bin server'"
